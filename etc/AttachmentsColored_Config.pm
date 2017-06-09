d068de2d81d6de0e731e582024fcc5b4d82331d0
toth.lajos <toth.lajos@docca.hu>
2017-06-09 09:03:04 +0200

    first version

---
+++ etc/AttachmentsColored_Config.pm
@@ -0,0 +1,11 @@
+Set(%AttachmentCategories, (
+'A' => 'background:#FF6347;'
+, 'B' => 'background:#90EE90;'
+, 'C' => 'background:#87CEFA;'
+, 'D' => 'background:#fCDC8C;'
+, 'hidden' => 'background:#DCDCDC;text-decoration:line-through'
+));
+
+Set($HideEmbeddedImages, 1);
+
+1;
---
+++ lib/RT/Extension/AttachmentsColored.pm
@@ -0,0 +1,33 @@
+package RT::Extension::AttachmentsColored;
+
+our $VERSION = '0.1';
+
+1;
+__END__
+
+=head1 NAME
+
+RT::Extension::AttachmentsColored - 
+
+=head1 DESCRIPTION
+
+tabbed ticket header
+
+=head1 INSTALLATION AND CONFIGURATION
+
+1. Enable the Extension in the SiteConfig, eg.:
+    Set(@Plugins, qw(RT::Extension::xyz RT::Extension::AttachmentsColored));
+
+2. Configure Extension
+insert this into siteconfig:
+TODO 
+
+=head1 AUTHOR
+
+bekeny balint & akos torok  C<< <docca@rt.docca.hu> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
