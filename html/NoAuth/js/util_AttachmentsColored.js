ded70e3bc144bdedee06be3b7e14649ab69a56c8
toth-lajos23 <toth.lajos@docca.hu>
2018-12-10 12:02:16 +0100

    Update util_AttachmentsColored.js

--- html/NoAuth/js/util_AttachmentsColored.js
+++ html/NoAuth/js/util_AttachmentsColored.js
@@ -1,7 +1,7 @@
 
 function toggleAttachmentsColored( Id, Ord, Cat, Action, AttId) {
     if ( jQuery(".attachtoggle-"+Id).length ) {
-	console.log("katt, load before");
+	// console.log("katt, load before");
 	jQuery(".attachtoggle-"+Id).load('<%RT->Config->Get('WebPath')%>/Helpers/Toggle/AttachmentsColored', {'id':Id, 'AttOrd': Ord, 'AttCat': Cat, 'AttAction': Action, 'AttId': AttId});
     }
 }
