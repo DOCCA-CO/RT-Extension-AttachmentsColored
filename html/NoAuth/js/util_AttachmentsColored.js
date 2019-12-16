a56dba236ea92f504e830e7e0d9f51386ce11634
PPetky <peter.petky@gmail.com>
2019-12-16 20:18:06 +0100

    16-12-2019 almost

--- html/NoAuth/js/util_AttachmentsColored.js
+++ html/NoAuth/js/util_AttachmentsColored.js
@@ -5,3 +5,15 @@ function toggleAttachmentsColored( Id, Ord, Cat, Action, AttId) {
 	jQuery(".attachtoggle-"+Id).load('<%RT->Config->Get('WebPath')%>/Helpers/Toggle/AttachmentsColored', {'id':Id, 'AttOrd': Ord, 'AttCat': Cat, 'AttAction': Action, 'AttId': AttId});
     }
 }
+
+function setFolder( attID, node ) {
+	console.log(attID + " *** " + node.value);
+	jQuery.ajax({
+		type: 'POST',
+		url: '<%RT->Config->Get('WebPath')%>/Ajax/AjaxModifyFolder',
+		data: {
+			'Value' : node.value,
+			'attachId' : attID
+		}
+	});
+}
