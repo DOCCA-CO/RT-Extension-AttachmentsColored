6793b09cae7cc2596593d5fa190b5a2ba80eaf6e
PPetky <50638874+PPetky@users.noreply.github.com>
2021-07-14 22:15:28 +0200

    Update AttachmentsColored_Config.pm
    #4679581

--- etc/AttachmentsColored_Config.pm
+++ etc/AttachmentsColored_Config.pm
@@ -10,8 +10,6 @@ Set($ConfigTicketID, 2);
 
 Set($DefaultFolderStructureTicketID, 2);
 
-Set($Scope, "(id = %d OR 'CF.{Munkaszám}' = '%s') AND (Status = '__Active__' OR Status = '__Inactive__')");
-
 Set(%RabbitMQ, ('enabled' => 0, 'host' => 'petky-dav', 'port' => 5672, 'login' => 'admin', 'passw' => 'admin'));
 
 1;
