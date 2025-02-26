5727c2f724e8ac99f615fe78f6ffad5c3754bd86
root <root@dox.horvathkft.hu>
2025-02-26 21:07:12 +0100

    picture preview fix

--- etc/AttachmentsColored_Config.pm
+++ etc/AttachmentsColored_Config.pm
@@ -6,9 +6,9 @@ Set(%AttachmentCategories, (
 	'hidden' => '#DCDCDC'
 ));
 
-Set($ConfigTicketID, 585);
+Set($ConfigTicketID, 2);
 
-Set($DefaultFolderStructureTicketID, 1513);
+Set($DefaultFolderStructureTicketID, 1);
 
 Set(%RabbitMQ, ('enabled' => 0, 'host' => '', 'port' => 5672, 'login' => '', 'passw' => ''));
 
